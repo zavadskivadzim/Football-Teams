befc12d1b0ef8b1a5aa46bc5d76d4b0365837683
Vadzim Zavadski <kp002210@mail.ru>
2022-05-30 11:45:08 +0300

    validation birthday

--- model/src/main/java/com/zavadski/model/Player.java
+++ model/src/main/java/com/zavadski/model/Player.java
@@ -5,8 +5,7 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 import static com.zavadski.model.constants.Constants.PLAYER_NAME_SIZE;
@@ -22,15 +21,17 @@ public class Player {
     private Integer playerId;
 
     @Column
-    @NotBlank(message = "Player name can't be empty")
+    @NotBlank(message = "Player name can not be empty")
     @Size(max = PLAYER_NAME_SIZE, message = "Player name must be less then {max} characters long")
     private String firstName;
 
     @Column
-    @NotBlank(message = "Player surname can't be empty")
+    @NotBlank(message = "Player surname can not be empty")
     private String surname;
 
     @Column
+    @Past(message = "Incorrect date of birthday")
+    @NotNull(message = "Birthday can not be empty")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(pattern = "yyyy-MM-dd",timezone="GMT+3")
     private LocalDate birthday;
